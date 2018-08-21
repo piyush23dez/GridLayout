5e189f298fbe021ca386d3a1ad09ff495f9c2d97
Piyush Sharma <piyush_sharma2@apple.com>
2018-08-20 21:55:26 -0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,54 @@
+
+import UIKit
+import  Foundation
+
+
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var button: UIButton!
+    
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+   
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let spacing: CGFloat = 16+16+16
+        return CGSize(width: (collectionView.frame.size.width-spacing)/2, height: (collectionView.frame.size.width-spacing)/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 70, left: 16, bottom: 0, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CollectionViewCell
+        cell?.backgroundColor = UIColor.red
+        cell?.button.setTitle("Cell \(indexPath.item)", for: .normal)
+        cell?.button.setTitleColor(.white, for: .normal)
+        return cell!
+    }
+}
